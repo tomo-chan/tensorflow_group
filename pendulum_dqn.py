662292ffc4f928c440d827ddf882d1689fcd3c4e
tomo-chan <aradgyma@gmail.com>
2016-12-01 03:33:30 +0900

    行動の離散化、教師信号の修正など

--- pendulum_dqn.py
+++ pendulum_dqn.py
@@ -4,24 +4,33 @@ from collections import deque
 import numpy as np
 import tensorflow as tf
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 DISCOUNT_RATE = 0.99
-EPISODE_NUM = 5000
+EPISODE_NUM = 1000
 BATCH_SIZE = 100
 EPSILON = 1
-EPSILON_DECAY = 0.00005
-HIDDEN_LAYERS = [200,200]
+EPSILON_MIN = 0.1
+EPSILON_DECAY = 0.005
+EXPLORATION = 1000
+HIDDEN_LAYERS = [100,100,100]
 EXP_SIZE = 1000
-
-replay_memory = deque(maxlen=EXP_SIZE)
+SKIP_TRAIN_COUNT = 10
+COPY_WEIGHT_COUNT = 20
 
 env = gym.make('Pendulum-v0')
 
+np.random.seed(100)
+
+replay_memory = deque(maxlen=EXP_SIZE)
+actions = np.asarray([env.action_space.low, env.action_space.high])
+
 STATE_NUM = env.observation_space.shape[0]
-ACTION_NUM = env.action_space.shape[0]
-ACTION_BOUND = env.action_space.high[0]
+ACTION_NUM = len(actions)
 
 tf.reset_default_graph()
+
+def leaky_relu(x,alpha=0.2):
+    return tf.maximum(alpha*x,x)
 
 def create_q_net(name, input, node_num):
     in_layer = input
@@ -32,7 +41,7 @@ def create_q_net(name, input, node_num):
         with tf.variable_scope(str(name)+'hidden'+str(i)) as scope:
             hidden_w = tf.Variable(tf.truncated_normal([node_num, n], stddev=0.01))
             hidden_b = tf.Variable(tf.zeros([n]))
-            out_layer = tf.nn.relu(tf.matmul(in_layer, hidden_w) + hidden_b)
+            out_layer = leaky_relu(tf.matmul(in_layer, hidden_w) + hidden_b)
             in_layer = out_layer
             node_num = n
             variables.append(hidden_w)
@@ -53,7 +62,7 @@ target = tf.placeholder(tf.float32, [None, STATE_NUM])
 tar_net, var_tar, bias_tar = create_q_net('tar_net', target, STATE_NUM)
 
 q_val = tf.placeholder(tf.float32, [None, ACTION_NUM])
-loss = tf.reduce_mean(tf.square(q_net - q_val))
+loss = tf.reduce_mean(tf.square(q_val - q_net))
 optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
 
 tar_ops = []
@@ -71,50 +80,62 @@ with tf.Session() as sess:
         observation = env.reset()
         total_reward = 0.0
         for t in range(200):
-            # env.render()
+            env.render()
             # 初回と2回目以降で配列の形が異なるのでここで整える
             observation = np.reshape(observation,[STATE_NUM])
             if np.random.random() < epsilon:
-                action = env.action_space.sample()
+                action_index = np.random.choice(ACTION_NUM, 1)[0]
             else:
-                action = sess.run(q_net, feed_dict={input: [observation]})[0]
+                action_index = action = np.argmax(sess.run(q_net, feed_dict={input: [observation]}))
             # action = [2.0] if action[0] > 0 else [-2.0]
             prev_observation = observation
-            epsilon = epsilon - EPSILON_DECAY if epsilon > 0 else epsilon
-            observation, reward, done, info = env.step(action)
-            replay_memory.append((prev_observation, action, reward, observation, done))
+            observation, reward, done, info = env.step(actions[action_index])
+            replay_memory.append((prev_observation, action_index, reward, observation, done))
             total_reward += reward
-            for k in range(5):
-                if len(replay_memory) <= BATCH_SIZE:
-                    break
-            # if (not t == 0 and t % (BATCH_SIZE-1) == 0) or done:
-                target_batch = []
-                input_batch = []
-                reward_batch = []
-                minibatch_size = min(len(replay_memory), BATCH_SIZE)
-                minibatch_indexes = np.random.choice(len(replay_memory), minibatch_size)
-                for i in minibatch_indexes:
-                    s_batch = replay_memory[i][0]
-                    a_batch = replay_memory[i][1]
-                    r_batch = replay_memory[i][2]
-                    ns_batch = replay_memory[i][3]
-                    done_batch = replay_memory[i][4]
+            # for k in range(1):
+            if EXP_SIZE <= len(replay_memory):
+                if (t+1) % SKIP_TRAIN_COUNT == 0 or done:
+                    input_batch = []
+                    action_batch = []
+                    reward_batch = []
+                    target_batch = []
+                    done_batch = []
+                    minibatch_size = min(len(replay_memory), BATCH_SIZE)
+                    minibatch_indexes = np.random.choice(len(replay_memory), minibatch_size)
+                    for i in minibatch_indexes:
+                        s_batch = replay_memory[i][0]
+                        a_batch = replay_memory[i][1]
+                        r_batch = replay_memory[i][2]
+                        ns_batch = replay_memory[i][3]
+                        d_batch = replay_memory[i][4]
 
-                    input_batch.append(s_batch)
-                    target_batch.append(ns_batch)
-                    reward_batch.append(r_batch)
+                        input_batch.append(s_batch)
+                        action_batch.append(a_batch)
+                        reward_batch.append(r_batch)
+                        target_batch.append(ns_batch)
+                        done_batch.append(d_batch)
 
-                input_batch = np.reshape(input_batch, [minibatch_size, STATE_NUM])
-                ns_batch = np.reshape(target_batch, [minibatch_size, STATE_NUM])
+                    input_batch = np.reshape(input_batch, [minibatch_size, STATE_NUM])
+                    ns_batch = np.reshape(target_batch, [minibatch_size, STATE_NUM])
+                    value_batch = sess.run(q_net, feed_dict={input: input_batch})
 
-                target_value = sess.run(tar_net, feed_dict={target: ns_batch})
-                target_batch = np.reshape(reward_batch, [minibatch_size, 1]) + DISCOUNT_RATE * target_value
+                    target_value = sess.run(tar_net, feed_dict={target: ns_batch})
+                    target_value_index = np.argmax(target_value, axis=1)
+                    for i in range(minibatch_size):
+                        value_batch[i][action_batch[i]] = reward_batch[i] if done_batch[i] else reward_batch[i] + DISCOUNT_RATE * target_value[i][target_value_index[i]]
 
-                sess.run(optimizer, feed_dict={input: input_batch, q_val: target_batch})
-                for op in tar_ops:
-                    sess.run(op)
+                    sess.run(optimizer, feed_dict={input: input_batch, q_val: value_batch})
+                    # print("loss: {}".format(sess.run(loss, feed_dict={input: input_batch, q_val: value_batch})))
 
-            if done:
-                break
+                    if t % COPY_WEIGHT_COUNT == 0:
+                        # print('before:{},{}'.format(sess.run(var_q[0])[0][0], sess.run(var_tar[0])[0][0]))
+                        for op in tar_ops:
+                            sess.run(op)
+                        # print('after:{},{}'.format(sess.run(var_q[0])[0][0], sess.run(var_tar[0])[0][0]))
+
+                    epsilon = epsilon - EPSILON_DECAY if epsilon > EPSILON_MIN and EXPLORATION < t+i_episode*200 else epsilon
+
+                    if done:
+                        break
         print("{},{}".format(i_episode+1, total_reward))
 
